50f0971be7838916367ee395b72cb9c3bc138b84
Tyler Lanigan <tylerlanigan@gmail.com>
2020-06-14 17:10:04 -0700

    changed auth audience to wetfoot

--- auth/auth.py
+++ auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'tylers-test.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee'
+API_AUDIENCE = 'wetfoot'
 
 
 class AuthError(Exception):
